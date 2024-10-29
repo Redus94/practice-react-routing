e62b211b9a61cc2ba028d278dfe75dd202d6ce22
Paweł Suder <pawel.suder94@gmail.com>
2024-10-29 11:47:52 +0100

    task 04 done

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Route, useHistory } from "react-router-dom";
+
+import Shop from "../src/components/Shop";
+import products from "../src/products.json";
 
 const Task04 = () => {
-    return (
-        <h1>Task04</h1>
-    );
-}
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    history.push(`/task04/${e.target.value}`);
+  };
+
+  return (
+    <>
+      <h1>Task04</h1>
+
+      <select onChange={handleChange} defaultValue={"none"}>
+        <option value="none">Wybierz sortowanie</option>
+        <option value="price-asc">Od najtańszego</option>
+        <option value="price-desc">Od najdroższego</option>
+      </select>
+
+      <Route path="/task04/price-asc">
+        <Shop list={[...products].sort((a, b) => a.price - b.price)} />
+      </Route>
+
+      <Route path="/task04/price-desc">
+        <Shop list={[...products].sort((a, b) => b.price - a.price)} />
+      </Route>
+    </>
+  );
+};
 
 export default Task04;
 
